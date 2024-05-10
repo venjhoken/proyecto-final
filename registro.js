b8a9130c5124cb9498ef68992fc0505f05c45353
Jhoss <jhosseth.paez.lp@gmail.com>
2024-05-09 23:29:53 -0500

    productos array

---
+++ registro.js
@@ -0,0 +1,604 @@
+var registro = [
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos gamer inalámbricos CAT STN-28 verde con luz LED.jpg",
+        "codigo": "AU01",
+        "precio": 20000,
+        "color": "Azul",
+        "marca": "Logitech",
+        "modelo": "CAT STN-28",
+
+    },
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos gamer inalámbricos Logitech G G Series G435 blanco crudo y lila.jpg",
+        "codigo": "AU02",
+        "precio": 30000,
+        "color": "blanco",
+        "marca": "Logitech",
+        "modelo": "G G Series G435",
+    },
+
+    {
+        "nombre": "Audífonos inalámbricos  ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos inalámbricos Lenovo X4 negro.jpg",
+        "codigo": "AU03",
+        "precio": 40000,
+        "color": "negro",
+        "marca": "Lenovo",
+        "modelo": "X4",
+
+    },
+
+    {
+        "nombre": "Audífonos inalámbricos  ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos inalámbricos Soundcore Life Series Life Q30 A3028 black.jpg",
+        "codigo": "AU04",
+        "precio": 30000,
+        "color": "negro",
+        "marca": "Soundcore",
+        "modelo": " Life Series Life Q30 A3028 ",
+    },
+
+    {
+        "nombre": "Audífonos in-ear gamer ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos in-ear gamer inalámbricos Lenovo LivePods LP6 negro con luz LED.jpg",
+        "codigo": "AU05",
+        "precio": 50000,
+        "color": "negro",
+        "marca": "Lenovo",
+        "modelo": "LivePods LP6 ",
+
+    },
+
+    {
+        "nombre": "Audífonos Internos Inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos Internos Inalámbricos Wi-c100 Color Negro.jpg",
+        "codigo": "AU06",
+        "precio": 30000,
+        "color": "negro",
+        "marca": "Lenovo",
+        "modelo": "Wi-c100",
+
+    },
+
+    {
+        "nombre": "Audífonos in-ear ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Audífonos Lenovo GM2 Pro.jpg",
+        "codigo": "AU07",
+        "precio": 80000,
+        "color": "negro",
+        "marca": "Lenovo",
+        "modelo": "GM2 Pro",
+
+    },
+
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Logitech G Pro X, Diadema Gamer 7.1 Profesional, Pc Ps4 Xbox Color Negro.jpg",
+        "codigo": "AU08",
+        "precio": 20000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": "G Pro X",
+
+    },
+
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/HYPERX Diadema Gamer Inalámbrica HyperX Cloud Flight PC PS5 PS4.jpg",
+        "codigo": "AU09",
+        "precio": 60000,
+        "color": "negro",
+        "marca": "HYPERX",
+        "modelo": "Cloud Flight",
+
+    },
+
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Logitech G535, Diadema Gamer Inalámbrica Lightspeed.jpg",
+        "codigo": "AU10",
+        "precio": 45000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": "G535",
+
+    },
+
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Logitech Zone Vibe 100 Diadema Profesional Inalámbrica bt Bl Color Blanco.jpg",
+        "codigo": "AU11",
+        "precio": 20000,
+        "color": "blanco",
+        "marca": "Logitech",
+        "modelo": "Zone Vibe 100",
+
+    },
+
+    {
+        "nombre": "Audífonos gamer inalámbricos ",
+        "categoria": "Audífonos",
+        "imagen": "imagen2/Skullcandy Crusher Wireless Over-the-Ear Headphones Black Coral S6CRW-K591.jpg",
+        "codigo": "AU12",
+        "precio": 65000,
+        "color": "negro",
+        "marca": "Skullcandy Crusher",
+        "modelo": "Coral S6CRW-K591",
+
+    },
+
+    {
+        "nombre": "Cámara web ",
+        "categoria": "Camaras",
+        "imagen": "imagen2/Cámara web Logitech 4K Pro Ultra HD Pro.jpg",
+        "codigo": "CAM01",
+        "precio": 200000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": "4K Pro Ultra HD Pro",
+
+    },
+
+    {
+        "nombre": "Cámara web ",
+        "categoria": "Camaras",
+        "imagen": "imagen2/Cámara Web Sat X11 720p Con Micrófono.jpg",
+        "codigo": "CAM02",
+        "precio": 250000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": "Sat X11 720p",
+
+    },
+
+    {
+        "nombre": "Cámara web USB",
+        "categoria": "Camaras",
+        "imagen": "imagen2/Webcan USB 2K Steren COM124.jpg",
+        "codigo": "CAM03",
+        "precio": 250000,
+        "color": "negro",
+        "marca": "Steren",
+        "modelo": "COM124",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado gamer Genius GX Gaming Scorpion K220 QWERTY.jpg",
+        "codigo": "TE01",
+        "precio": 30000,
+        "color": "negro",
+        "marca": "Genius GX",
+        "modelo": "Scorpion K220 QWERTY",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado gamer HyperX Alloy Origins 60 QWERTY HyperX Lineal Red inglés US color negro con luz RGB.jpg",
+        "codigo": "TE02",
+        "precio": 150000,
+        "color": "negro",
+        "marca": "HyperX",
+        "modelo": "Alloy Origins 60",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado Gamer Razer Ornata V3 X Black Edition.jpg",
+        "codigo": "TE03",
+        "precio": 70000,
+        "color": "negro",
+        "marca": "Razer",
+        "modelo": "Ornata V3 X ",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Logitech K400 Plus, Teclado Inalámbrico Touch, Pc Android Tv Color del teclado Negro Idioma Español.jpg",
+        "codigo": "TE04",
+        "precio": 100000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": "K400 Plus",
+
+    },
+    
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado Gamer Xtrike Me USB.jpg",
+        "codigo": "TE05",
+        "precio": 60000,
+        "color": "negro",
+        "marca": "XTRIKE ME",
+        "modelo": "KB-509",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado Gamer Xtech Armiger USB.jpg",
+        "codigo": "TE06",
+        "precio": 50000,
+        "color": "negro",
+        "marca": "XTECH",
+        "modelo": "XTK-510S",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/TECLADO GAMER ALAMBRICO WYCK WK-220.jpg",
+        "codigo": "TE07",
+        "precio": 70000,
+        "color": "blanco",
+        "marca": "WYCK",
+        "modelo": "WK-220",
+
+    },
+
+    {
+        "nombre": "Teclado gamer ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado Gamer Gxt 830 Rw Avonn.jpg",
+        "codigo": "TE08",
+        "precio": 80000,
+        "color": "negro",
+        "marca": "TRUST",
+        "modelo": "Gxt 830",
+
+    },
+
+    {
+        "nombre": "Teclado Inalámbrico Bluetooth ",
+        "categoria": "Teclados",
+        "imagen": "imagen2/Teclado Inalámbrico Bluetooth K380 Logitech.jpg",
+        "codigo": "TE09",
+        "precio": 80000,
+        "color": "blanco",
+        "marca": "Logitech",
+        "modelo": "K380",
+    },
+
+    {
+        "nombre": "Monitor gamer ",
+        "categoria": "Monitores",
+        "imagen": "imagen2/Monitor Samsung Ips De 24 Full Hd Freesync 75Hz Hdmi F24t350.jpg",
+        "codigo": "MO01",
+        "precio": 800000,
+        "color": "negro",
+        "marca": "Samsung",
+        "modelo": "Ips De 24 Full Hd Freesync 75Hz Hdmi F24t350",
+
+    },
+    
+    {
+        "nombre": "Monitor gamer ",
+        "categoria": "Monitores",
+        "imagen": "imagen2/Monitor LG Gamer Ultragear Fhd 24 - 165 Hz Color Negro.jpg",
+        "codigo": "MO02",
+        "precio": 700000,
+        "color": "negro",
+        "marca": "LG",
+        "modelo": "Ultrawide LG 29 Ips Hdr10 Freesync 75hz 5ms 29wp500",
+
+    },
+
+    {
+        "nombre": "Monitor Ultrawide ",
+        "categoria": "Monitores",
+        "imagen": "imagen2/Monitor Ultrawide LG 29 Ips Hdr10 Freesync 75hz 5ms 29wp500 Color Negro.jpg",
+        "codigo": "MO03",
+        "precio": 900000,
+        "color": "negro",
+        "marca": "LG",
+        "modelo": "29 Ips Hdr10 Freesync 75hz 5ms 29wp500",
+
+    },
+
+    {
+        "nombre": "Mouse gamer ",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse gamer de juego Razer DeathAdder Essential negro.jpg",
+        "codigo": "RA01",
+        "precio": 180000,
+        "color": "negro",
+        "marca": "Razer ",
+        "modelo": "DeathAdder Essential",
+
+    },
+
+    {
+        "nombre": "Mouse gamer ",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse gamer de juego VSG Aurora púrpura austral.jpg",
+        "codigo": "RA02",
+        "precio": 170000,
+        "color": "púrpura",
+        "marca": "Razer ",
+        "modelo": " VSG Aurora ",
+
+    },
+
+    {
+        "nombre": "Mouse gamer ",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Logitech G203 Rgb Lighsync 6 Botones 8000 Dpi.jpg",
+        "codigo": "RA03",
+        "precio": 150000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": " G203 Rgb Lighsync",
+
+    },
+
+
+
+    {
+        "nombre": "Mouse gamer Ergonómico  ",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Genius Inalambrico Ergonomico Vertical 8250s Neg- Gris.jpg",
+        "codigo": "RA06",
+        "precio": 170000,
+        "color": "negro",
+        "marca": "GENIUS",
+        "modelo": "ERGO 8250S",
+
+    },
+
+    {
+        "nombre": "Mouse gamer Ergonómico  ",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gaming Vertical Ergonomico Programable Gamer Zelotes T30.jpg",
+        "codigo": "RA07",
+        "precio": 140000,
+        "color": "negro",
+        "marca": "ZELOTES",
+        "modelo": "T30",
+
+    },
+
+    {
+        "nombre": "Mouse gamer Cableado  ",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Logitech G G502 Cableado.jpg",
+        "codigo": "RA08",
+        "precio": 130000,
+        "color": "negro",
+        "marca": "Logitech",
+        "modelo": "1040803",
+
+    },
+
+    {
+        "nombre": "Mouse gamer Alambrico",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Logitech USB G502X Alámbrico 25K DPI  Mouse ergonómico.jpg",
+        "codigo": "RA09",
+        "precio": 110000,
+        "color": "blanco",
+        "marca": "Logitech",
+        "modelo": "G502X ",
+
+    },
+
+    {
+        "nombre": "Mouse gamer",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Redragon M988 Storm Elite RGB Blanco.jpg",
+        "codigo": "RA10",
+        "precio": 159900,
+        "color": "blanco",
+        "marca": "REDRAGON",
+        "modelo": "M988W-RGB",
+
+    },
+
+ 
+    {
+        "nombre": "Mouse gamer",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Redragon Griffin M607 con luces RGB.jpg",
+        "codigo": "RA12",
+        "precio": 99900,
+        "color": "negro",
+        "marca": "REDRAGON",
+        "modelo": "M607",
+
+    },
+
+    {
+        "nombre": "Mouse gamer",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Redragon M601-rgb Centrophorus.jpg",
+        "codigo": "RA13",
+        "precio": 79900,
+        "color": "negro",
+        "marca": "REDRAGON",
+        "modelo": "m601 rgb",
+
+    },
+
+    {
+        "nombre": "Mouse gamer",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Programable RGB USB 7 Botones 6400 DPI Xtrike Me GM.jpg",
+        "codigo": "RA14",
+        "precio": 54900,
+        "color": "negro",
+        "marca": "XTRIKE ME",
+        "modelo": "GM-512",
+
+    },
+
+    {
+        "nombre": "Mouse gamer",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Delux M800BU RGB.jpg",
+        "codigo": "RA15",
+        "precio": 55900,
+        "color": "negro",
+        "marca": "DELUX",
+        "modelo": "M800BU RGB",
+
+    },
+
+    {
+        "nombre": "Mouse gamer",
+        "categoria": "Mouses",
+        "imagen": "imagen2/Mouse Gamer Zelotes T-80 7200 Dpi Usb Cable 7 Botones.jpg",
+        "codigo": "RA16",
+        "precio": 89900,
+        "color": "negro",
+        "marca": "ZELOTES",
+        "modelo": "T-80",
+
+    },
+
+    {
+        "nombre": "Impresora a color ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora a color multifunción Canon Pixma G3160 con wifi.jpg",
+        "codigo": "IMP01",
+        "precio": 89900,
+        "color": "negro",
+        "marca": "Canon",
+        "modelo": "Pixma G3160",
+
+    },
+
+    {
+        "nombre": "Impresora a color ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora Epson EcoTank L800 (110V).jpg",
+        "codigo": "IMP02",
+        "precio": 89900,
+        "color": "negro",
+        "marca": "Epson",
+        "modelo": "EcoTank L800 (110V)",
+
+    },
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora Hp Multifuncional Smart Tank 520 Smart Tasks Usb Color Blanco.jpg",
+        "codigo": "IMP03",
+        "precio": 89900,
+        "color": "blanco",
+        "marca": "Hp",
+        "modelo": "Smart Tank 520 ",
+
+    },
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora Multifuncional Epson EcoTank L3150.jpg",
+        "codigo": "IMP04",
+        "precio": 89900,
+        "color": "negro",
+        "marca": "Epson",
+        "modelo": "EcoTank L3150 ",
+
+    },
+
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora multifuncional HP DeskJet Ink Advantage 2875.jpg",
+        "codigo": "IMP05",
+        "precio": 370000,
+        "color": "negro",
+        "marca": "HP",
+        "modelo": "DeskJet Ink Advantage 2875 ",
+
+    },
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora Multifuncional Canon G3160.jpg",
+        "codigo": "IMP06",
+        "precio": 500000,
+        "color": "negro",
+        "marca": "CANON",
+        "modelo": "G3160",
+
+    },
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora brother dcpt720dw multifuncional wifi.jpg",
+        "codigo": "IMP07",
+        "precio": 999000,
+        "color": "negro",
+        "marca": "BROTHER",
+        "modelo": "dcpt720dw",
+
+    },
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/Impresora brother MFC-T920DW Multifuncional.jpg",
+        "codigo": "IMP08",
+        "precio": 1200000,
+        "color": "negro",
+        "marca": "BROTHER",
+        "modelo": "MFC-T920DW",
+
+    },
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/IMPRESORA BROTHER MFC-L9570CDW MULTIFUNCIONAL.jpg",
+        "codigo": "IMP09",
+        "precio": 4000000,
+        "color": "blanco",
+        "marca": "BROTHER",
+        "modelo": "MFC-L9570CDW",
+
+    },
+
+
+    {
+        "nombre": "Impresora Multifuncional  ",
+        "categoria": "Impresoras",
+        "imagen": "imagen2/IMPRESORA EPSON WORKFORCE PRO WF-6590 MULTIFUNCIONAL.jpg",
+        "codigo": "IMP10",
+        "precio": 4000000,
+        "color": "blanco",
+        "marca": "EPSON",
+        "modelo": "WF-6590",
+
+    },
+] 
+
+//document.getElementById("Nombre").value = registro[].nombre;
